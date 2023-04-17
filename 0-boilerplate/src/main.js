ae73b36750bcc17c32a238e1d292223ba2f3f134
chayeoi <chayeoikeem@gmail.com>
2023-04-17 18:11:45 +0900

    feat(0-boilerplate): Boilerplate 추가

---
+++ 0-boilerplate/src/main.js
@@ -0,0 +1,46 @@
+import * as THREE from 'three';
+
+window.addEventListener('load', function () {
+  init();
+});
+
+function init() {
+  const renderer = new THREE.WebGLRenderer({
+    antialias: true,
+  });
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  document.body.appendChild(renderer.domElement);
+
+  const scene = new THREE.Scene();
+
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    1,
+    500,
+  );
+
+  camera.position.z = 5;
+
+  render();
+
+  function render() {
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(render);
+  }
+
+  function handleResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    renderer.render(scene, camera);
+  }
+
+  window.addEventListener('resize', handleResize);
+}
